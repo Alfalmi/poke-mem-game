7a9fd3b50a050a8e0ed0884283e5e6c906976f9a
Alfalmi <pinooxd@gmail.com>
2022-03-17 19:14:04 -0700

    api

---
+++ src/theme/Theme.js
@@ -0,0 +1,15 @@
+export const Theme = {
+  colors: {
+    light: '#eee',
+    lightHover: '#ccc',
+    lightActive: '#aaa',
+    dark: '#383838',
+    darkHover: '#272727',
+    darkActive: '#161616',
+    green: '#99ff66',
+    red: '#ffc2b3',
+    pokeballRed: '#ff0000',
+  },
+}
+
+export default Theme
